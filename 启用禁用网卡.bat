b8da381cca99459c90cd148e7b01fba4ea0cf218
michealhansun <hansun@live.com>
2023-07-07 09:08:46 +0800

    Update 启用禁用网卡.bat

--- 启用禁用网卡.bat
+++ 启用禁用网卡.bat
@@ -1,6 +1,8 @@
 @echo off
-netsh interface set interface "WLAN 2" disabled #关闭指定网卡
-netsh interface set interface "以太网 2" enabled #启用指定网卡
-netsh interface show interface #查询网卡名称和查询状态
-echo 已禁用无线网卡，启用有线网卡
+netsh interface set interface "以太网" disabled
+netsh interface set interface "WLAN 3" enabled
+netsh interface set interface "WLAN 3" disabled
+netsh interface set interface "以太网" enabled
+netsh interface show interface
+echo
 pause
